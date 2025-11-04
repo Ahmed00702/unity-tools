cfd6b8e5e4eaafe14891d11614973ff7a44d50aa
Ahmed Shahin <107210671+Ahmed00702@users.noreply.github.com>
2025-11-04 20:36:40 +0300

    Add copyright notice and license information

--- AudioTrimmerWindow.cs
+++ AudioTrimmerWindow.cs
@@ -1,3 +1,8 @@
+// Copyright (c) 2025 Ahmed Shahin
+// This script is part of the Unity Scripts collection.
+// Licensed under the MIT License. See LICENSE file in the project root for details.
+
+
 using UnityEngine;
 using UnityEditor;
 using System.IO;
